dc42ab83b144cd5c8f94b5b84cd846c2c5d244db
Mariu7 <marius.kvasnauskas@gmail.com>
2020-03-31 23:50:32 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('this is working');
+})
+
+
+app.listen(3000, ()=> {
+    console.log('App is running on port 3000');
+})
+
+
+/* TODO:
+/ res = this is working
+/singin --> POST  = success/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT --> user
+*/
