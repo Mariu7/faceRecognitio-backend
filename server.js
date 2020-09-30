0deeb7f50085ce6ade001deb7f067f44db452ba0
Mariu7 <marius.kvasnauskas@gmail.com>
2020-10-01 00:46:06 +0300

    bcrypt

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// 278
+// 281-283
 import express from 'express';
 
 const app = express();
@@ -26,29 +26,62 @@ const database = {
 }
 
 app.get('/', (req, res) => {
-    res.send('this is working');
+    res.send(database.users);
 })
 
 app.post('/signin', (req, res) => {
     if (req.body.email === database.users[0].email &&
         req.body.password === database.users[0].password) {
-            res.json('succsess');
+            res.json('success');
     }   else {
             res.status(400).json('Error logging in');
         }
     
 })
 
+app.post('/register', (req,res) => {
+    const {email, name, password} = req.body;
+    database.users.push({        
+            id: '125',
+            name: name,
+            email: email,
+            password: password,
+            entries: 0,
+            joined: new Date()        
+    })
+    res.json(database.users[database.users.length-1]);
+})
+
+app.get('/profile/:id', (req,res) => {
+    const {id} = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            return res.json(user);
+        } 
+    })
+    if(!found) {
+        res.status(400).json('not found');
+    }
+})
+
+app.put('/image', (req,res) => {
+    const {id} = req.body;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            user.entries++;
+            return res.json(user.entries);
+        } 
+    })
+    if(!found) {
+        res.status(400).json('not found');
+    }
+
+})
 
 app.listen(3001, ()=> {
     console.log('App is running on port 3001');
 })
-
-
-/* TODO:
-/ res = this is working
-/singin --> POST  = success/fail
-/register --> POST = user
-/profile/:userId --> GET = user
-/image --> PUT --> user
-*/
