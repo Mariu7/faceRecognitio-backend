55e22d38e0527ed3028d437ca9e70c393afeac8f
Mariu7 <marius.kvasnauskas@gmail.com>
2020-09-30 23:55:39 +0300

    Update server.js
    278

--- server.js
+++ server.js
@@ -1,14 +1,47 @@
+// 278
 import express from 'express';
 
 const app = express();
+app.use(express.json());
+//"Database" for now
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'John',
+            email: 'john@gmail.com',
+            password: 'cookies',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Sally',
+            email: 'sally@gmail.com',
+            password: 'bananas',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
 
 app.get('/', (req, res) => {
     res.send('this is working');
 })
 
+app.post('/signin', (req, res) => {
+    if (req.body.email === database.users[0].email &&
+        req.body.password === database.users[0].password) {
+            res.json('succsess');
+    }   else {
+            res.status(400).json('Error logging in');
+        }
+    
+})
 
-app.listen(3000, ()=> {
-    console.log('App is running on port 3000');
+
+app.listen(3001, ()=> {
+    console.log('App is running on port 3001');
 })
 
 
